49ab406c9376129d53cde59db52fa17ca7cd8632
zjx-immersion <jianxin_home@163.com>
2018-11-10 11:39:06 +0800

    [Jason]add the test process in per test

--- src/test/java/ExpenseService/ExpenseServiceTest.java
+++ src/test/java/ExpenseService/ExpenseServiceTest.java
@@ -12,26 +12,36 @@ import static org.assertj.core.api.Assertions.catchThrowable;
 class ExpenseServiceTest {
     @Test
     void should_return_internal_expense_type_if_project_is_internal() throws UnexpectedProjectTypeException {
-
+        // given
+        // when
+        // then
     }
 
     @Test
     void should_return_expense_type_A_if_project_is_external_and_name_is_project_A() throws UnexpectedProjectTypeException {
-
+        // given
+        // when
+        // then
     }
 
     @Test
     void should_return_expense_type_B_if_project_is_external_and_name_is_project_B() throws UnexpectedProjectTypeException {
-
+        // given
+        // when
+        // then
     }
 
     @Test
     void should_return_other_expense_type_if_project_is_external_and_has_other_name() throws UnexpectedProjectTypeException {
-
+        // given
+        // when
+        // then
     }
 
     @Test
     void should_throw_unexpected_project_exception_if_project_is_invalid() {
-
+        // given
+        // when
+        // then
     }
 }
