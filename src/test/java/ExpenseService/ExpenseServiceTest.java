5a03250b4b2c3a1f9caff6b0741445c9ee048822
zjx-immersion <jianxin_home@163.com>
2018-11-10 11:31:26 +0800

    [Jason]init a practice stack for unit test

---
+++ src/test/java/ExpenseService/ExpenseServiceTest.java
@@ -0,0 +1,37 @@
+package ExpenseService;
+
+import ExpenseService.Exception.UnexpectedProjectTypeException;
+import ExpenseService.Expense.ExpenseType;
+import ExpenseService.Project.Project;
+import ExpenseService.Project.ProjectType;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+class ExpenseServiceTest {
+    @Test
+    void should_return_internal_expense_type_if_project_is_internal() throws UnexpectedProjectTypeException {
+
+    }
+
+    @Test
+    void should_return_expense_type_A_if_project_is_external_and_name_is_project_A() throws UnexpectedProjectTypeException {
+
+    }
+
+    @Test
+    void should_return_expense_type_B_if_project_is_external_and_name_is_project_B() throws UnexpectedProjectTypeException {
+
+    }
+
+    @Test
+    void should_return_other_expense_type_if_project_is_external_and_has_other_name() throws UnexpectedProjectTypeException {
+
+    }
+
+    @Test
+    void should_throw_unexpected_project_exception_if_project_is_invalid() {
+
+    }
+}
